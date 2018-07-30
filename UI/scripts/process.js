aa68c08c01f0c9ae5fce4cf685230c61c3801c92
sidd-92 <sidddharthvn2014@gmail.com>
2018-07-29 19:32:27 -0700

    added next game functionlity when a word is complete using MODAL

--- UI/scripts/process.js
+++ UI/scripts/process.js
@@ -9,6 +9,7 @@ let givenAnswer;
 let message = document.getElementById("error");
 let success = document.getElementById("success");
 
+
 let textInput = document.getElementById("getWord");
 function getInput() {
   givenAnswer = textInput.value.toLowerCase();
@@ -20,6 +21,7 @@ buttonClick.addEventListener("click", checkWord);
 let displayAnswers = document.getElementById("displayAnswers");
 //let element;
 function createList(givenText) {
+  
   var node = document.createElement("P"); // Create a <p> node
   $(node).addClass("answers");
   var textnode = document.createTextNode(givenText); // Create a text node
@@ -40,47 +42,67 @@ clearField.addEventListener("click", function() {
  * -----------------------------------------------------------------------
  */
 
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+ //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 let answeredBy = {};
-let subWordCount = 0;
+  let subWordCount = 0;
 
 function checkWord() {
-  success.innerHTML = "";
-  getInput();
-  if (givenAnswer !== "") {
-    if (
-      message.innerHTML == "ERROR!" ||
-      message.innerHTML == "Not the word I want, Try Again !" ||
-      message.innerHTML == "You already got that word!"
-    ) {
-      message.innerHTML = "";
-    }
-    //console.log(givenAnswer);
-    //displayAnswers.childNodes[1].innerHTML = givenAnswer;
-    // success.innerHTML = "All Words Complete";
-    if (givenAnswer in answeredBy == false) {
-      if (givenAnswer in assignedAnswers) {
-        //console.log("Assigned Answers", assignedAnswers);
-        subWordCount++;
-        if (subWordCount == totalAns) {
-          success.innerHTML = "Congrats ! Level Complete";
-        } else {
-          success.innerHTML = "Yes!, You Got It";
-        }
-
-        answeredBy[givenAnswer] = givenAnswer;
-        createList(givenAnswer);
+    
+    success.innerHTML = "";
+    getInput();
+    if (givenAnswer !== "") {
+      if (
+        message.innerHTML == "ERROR!" ||
+        message.innerHTML == "Not the word I want, Try Again !" ||
+        message.innerHTML == "You already got that word!"
+      ) {
+        message.innerHTML = "";
+      }
+      //console.log(givenAnswer);
+      //displayAnswers.childNodes[1].innerHTML = givenAnswer;
+      // success.innerHTML = "All Words Complete";
+      if (givenAnswer in answeredBy == false) {
+         if (givenAnswer in assignedAnswers) {
+            //console.log("Assigned Answers", assignedAnswers);
+           subWordCount++;
+           if (subWordCount == totalAns) {
+             //success.innerHTML = "Congrats ! Level Complete";
+				addOverlay();	   
+           }else{success.innerHTML = "Yes!, You Got It";}
+				console.log("GIVEN ANSWERS:",givenAnswer);
+            
+            answeredBy[givenAnswer] = givenAnswer;
+            createList(givenAnswer);
+           
+          } else {
+            message.innerHTML = "Not the word I want, Try Again !";
+          }
+         
       } else {
-        message.innerHTML = "Not the word I want, Try Again !";
+        message.innerHTML = "You already got that word!";
       }
     } else {
-      message.innerHTML = "You already got that word!";
+      message.innerHTML = "ERROR!";
     }
-  } else {
-    message.innerHTML = "ERROR!";
+  
+    textInput.value = null;
   }
 
-  textInput.value = null;
+var modal = document.getElementById('myModal');
+var span = document.getElementsByClassName("close")[0];
+
+function addOverlay(){
+    modal.style.display = "block";
 }
 
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
--- WordGame/wordGame/description.md
+++ WordGame/wordGame/description.md
@@ -1,70 +1,29 @@
-# <center>The Word Scramble</center>
+# <center>The Word UnScramble</center>
 ## **<center>Overview</center>** 
-
-LIST OF MODULES NEEDED
-1. PLAY GAME - MAIN PAGE
-        1.1 PICK A WORD
-        1.2 WAIT TO SOLVE
-            1.2.1 SHOW JUMBLED
-            1.2.2 GET WORD FROM USER
-            1.2.3 HANDLE ANSWER 
-
 ---
-A Small word game which is same like the word cookie game in the mobile phone game.The user will be provided with a list of letters and needs to guess the word from those letters.
+An amazing word game where a user is given some jumbled letters and the user has to guess the words from it.
+---
+# Versions Roadmap
+---
+## Version 0 Changes
+- [X] **Used Command Line Interface to ask questions**
+- [X] **Changed the flow of the program and make it more readable and seperate**
+- [X] **Using modules/functions for each logic**
+- [X] **Made the main game function flow easy**
+---
+## Version 1.0 Changes to do
+- [ ] **Using vanilla Js and front end technology**
+- [ ] **Using external link to fetch data only once**
+- [ ] **adding responsivness for most of the devices and browsers**
+- [ ] **adding material design concepts by google**
 ---
 ---
-# Changes To Be Made
- - [X] **Using Command Line Interface to ask questions**
-- [ ] **Change the flow of the program and make it more readable and seperate**
-- [ ] **Use modules/functions for each logic**
-- [ ] **Make the main game function flow easy**
+## Version 1.1 Changes to do
+- [ ] **Convert to react app**
+- [ ] **use more words**
+- [ ] **add animations**
+- [ ] **add music and sounds**
 ---
-## **Goals**
----
-1. Create a working model of the version 0 of the game
-2. Contains a few words and user will be able to guess words the same way
-3. This game should use angular as the front end
-4. This game will have a scoring system also
----
-## **Program Specifications**
----
-1. Tasks
-    1. Keep track of states
-        1. State 1 : *Start of the game*
-        2. State 2 : *Middle of the game*
-        3. State 3 : *End of the game*
-2. Basic Terminology
-    1. *levelManager* : keeps track of the levels
-    2. *scoreManager* : Keeps track of the scores in each level
-    3. *stateManager* : keeps track of the words completed
-    4. *subWordCompleted* : keeps track of whether the sub word is completed
-
----
-# <center> **Game Mechanism**
-
-
-**State 1 : Start of game**
-
-- The user will be provided a list of jumbled letters.
-    - To do this you need to “shuffle” the letters    
-- User will provide a word from that list of letters
-
-
-**State 2 : middle of game**
-
-- if the user is right from the dictionary of words, then got to stage 3, otherwise go to ii
-- if the user is wrong then give him a another chance, currently not taking much into account for the number of tries.
-
-**State 3 : end of game**
-
-- If the user is getting the words right, then track the score using **“ScoreManager”**
-- If the score is equal to the length of the array then he got all the words
-- retain the letters until all the words are completed: 
-***levelManager : level 1 = givenWord***
-- As soon as the ***"subWordCompleted".length == answers.length***
-- set ***stateManager = level 1 = giveWord***
-- set levelManager to next random word, excluding the level 1
-
 
 
 
